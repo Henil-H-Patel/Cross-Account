1c9b45ac59203b29cf66c1c510575bb11c4b1353
HenilPatel <henilpatel2020@gmail.com>
2024-12-11 15:34:38 -0600

    updated code

--- terraform.tfvars
+++ terraform.tfvars
@@ -4,6 +4,6 @@ private_subnet_1a = "172.25.0.0/18"
 private_subnet_1b = "172.25.64.0/18"
 public_subnet_1a = "172.25.128.0/18"
 public_subnet_1b = "172.25.192.0/18"
-STAGE = "QA"
+# STAGE = "QA"
 
 
--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "private_subnet_1a" { }
 variable "private_subnet_1b" { }
 variable "public_subnet_1a" { }
 variable "public_subnet_1b" { }
-variable "STAGE" { }
+# variable "STAGE" { }
 # variable "vpc_cidr-2" { }
 # variable "private_subnet_1a-2" { }
 # variable "private_subnet_1b-2" { }
