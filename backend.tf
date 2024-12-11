17e72d832a1401119e39dfad7bc8c854c623db26
HenilPatel <henilpatel2020@gmail.com>
2024-12-11 16:04:11 -0600

    remove the backend file

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "prod-new-vpc"
-    key            = "terraform.tfstate"
-    region         = "us-west-2" #***
-    encrypt        = true
-   
-  }
-}
